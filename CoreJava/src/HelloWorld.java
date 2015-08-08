7b91cf33b3e982da0fd26caf1681e8aa908778e2
Jayram <jayram.rout@gmail.com>
2015-08-07 20:45:54 -0400

    Update

--- CoreJava/src/HelloWorld.java
+++ CoreJava/src/HelloWorld.java
@@ -1,6 +1,4 @@
-
 public class HelloWorld {
-//	public static void main(String args[]) {
 	static public void main(String jayram[]) {
 		System.out.println("Hello...Jayram");
 		System.out.println("With some modification...");
